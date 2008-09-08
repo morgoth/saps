7f0e67e68375e1eb648e9d2a9daf6602bb9cfdeb
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2008-09-09 00:40:31 +0200

    gibberrish installed

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
 	include AuthenticatedSystem
+	around_filter :set_language
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '181196128ea589718763da4c051f5969'
@@ -12,4 +13,10 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+	private
+	  def set_language
+      	Gibberish.use_language(:pl) { yield }
+    end
+
+	
 end

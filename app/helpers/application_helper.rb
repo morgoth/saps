eb25e8a180d0f6f29f9fd7d090ee6d882649f768
ringwraith <ringwraith@wp.pl>
2008-09-11 06:19:26 -0700

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,10 @@ module ApplicationHelper
     date.strftime("%y.%m.%d")
   end
   
+  def current(lang)
+    {:controller=>controller.controller_name, :action => controller.action_name, :locale =>lang, :id=>params[:id], :league_id=>params[:league_id]}
+  end
+  
   def leagues?
     flag = false
     %w{teams leagues matches rounds}.each do |check|

71d10afaf8999b23a827232333e56fe8c6b5cf6c
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-01-17 14:43:28 +0100

    herokugarden testing

--- config/environment.rb
+++ config/environment.rb
@@ -19,8 +19,8 @@ Rails::Initializer.run do |config|
 	# config.gem "xml-simple"
 	config.gem "mislav-will_paginate", :lib => "will_paginate", :version => ">= 2.3.6"
 	config.gem  "thoughtbot-paperclip", :lib => "paperclip", :version => ">= 2.2.2"
-	# config.gem "rspec", :lib =>"spec"
-	# config.gem "rspec-rails", :lib => "spec"
+	#config.gem "rspec", :lib =>"spec", :version => ">= 1.1.12"
+	#config.gem "rspec-rails", :lib => "spec", :version => ">= 1.1.12"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,21 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Match do
-  AVAILABLE_SCORES = %w{3:0 3:1 3:2 0:3 1:3 2:3}
-  AVAILABLE_SCORES << ''
   before(:each) do
     @valid_attributes = {
     :round_id => 1, :home_team_id => 1, :visitor_team_id => 2, :score => ''
     }
     @match=Match.new @valid_attributes
-    @team1=Team.create({:id=>1, :name=>'one'})
-    @team2= Team.create({:id=>2, :name=>'two'})
 end
 it "should be valid with valid score" do 
-  AVAILABLE_SCORES.each do |score|
-    @match.score= score  
-    @match.should be_valid
-  end
+	pending
 end
 
 

da6e08aa4707a7f3f0373a97c35554332296e1ad
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-04-16 17:06:10 +0200

    remove photo from players because of heroku read only system

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,11 @@
 class Player < ActiveRecord::Base
-	has_attached_file :photo, :url  => "/images/players_photo/:basename.:extension",
-																				 :path => ":rails_root/public/images/players_photo/:basename.:extension"
+	#has_attached_file :photo, :url  => "/images/players_photo/:basename.:extension",
+	#																			 :path => ":rails_root/public/images/players_photo/:basename.:extension"
 	
 	POSITIONS = %w{Przyjmujący Atakujący Rozgrywający Środkowy Libero}
   
-	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
-	validates_attachment_size :photo, :less_than => 30.kilobytes
+	#validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+	#validates_attachment_size :photo, :less_than => 30.kilobytes
 	validates_presence_of :name
 	validates_uniqueness_of :name
 	validates_inclusion_of :position, :in => POSITIONS, :message => "Wrong position", :unless => :blank_position

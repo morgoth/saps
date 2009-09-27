6f71f72d801b79d464b6d1012b13cbf30cb33b28
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2009-09-27 11:15:22 +0200

    fixing last commit

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,10 +1,9 @@
 class Player < ActiveRecord::Base
   POSITIONS = %w{Przyjmujący Atakujący Rozgrywający Środkowy Libero}
 
-  #default_scope :order => "name"
+  default_scope :order => "last_name"
 
-  #validates_presence_of :name
-  #validates_uniqueness_of :name
+  validates_presence_of :first_name, :last_name
   validates_inclusion_of :position, :in => POSITIONS, :message => "Wrong position", :allow_blank => true
   validates_numericality_of :height, :only_integer => true, :greater_than => 100, :less_than => 230, :allow_nil => true
 

68a85cb5ac50d81b90a552940a63a299f9abe4e3
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2010-08-13 22:21:16 +0200

    update to rails3 RC

--- config/application.rb
+++ config/application.rb
@@ -8,43 +8,39 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Saps
   class Application < Rails::Application
-
     require Rails.root.join('lib/google_analytics')
-    config.time_zone = 'Warsaw'
-
-    config.i18n.load_path += Dir[File.join(Rails.root, "config", "locales", "**", "*.{rb,yml}")]
-    config.i18n.default_locale = :pl
-    config.middleware.use Rack::GoogleAnalytics, "UA-7563082-2"
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{config.root}/extras )
+    config.middleware.use Rack::GoogleAnalytics, "UA-7563082-2"
+
+    # Custom directories with classes and modules you want to be autoloadable.
+    # config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
+    # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    # Activate observers that should always be running
+    # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Configure generators values. Many other options are available, be sure to check the documentation.
     config.generators do |g|
-      g.orm                  :active_record
       g.template_engine      :haml
       g.test_framework       :test_unit
       g.fixtures_replacement :factory_girl
     end
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = "Warsaw"
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.i18n.default_locale = :pl
+
+    # JavaScript files you want as :defaults (application.js is always included).
+    config.action_view.javascript_expansions[:defaults] = %w(jquery jquery-ui rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"

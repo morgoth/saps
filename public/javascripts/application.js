d7d1228f007a445c982f33154cb97122782c138a
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-05-15 22:49:34 +0200

    removed unused code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,20 +1,15 @@
-$(document).ready(
-  function() {
-    $("input#user_session_login").focus();
-    $('#jsddm > li').bind('mouseover', jsddm_open);
-    $('#jsddm > li').bind('mouseout',  jsddm_timer);
+jQuery(function($) {
+    $('#menu > li').bind('mouseover', jsddm_open);
+    $('#menu > li').bind('mouseout',  jsddm_timer);
 
-    $("#new_comment").submitWithAjax();
-
-    $("#players img").click( function() {
-      if ($(this).next(".hidden").is(":hidden")) {
-        $(this).next(".hidden").slideDown("normal");
-      } else {
-        $(this).next(".hidden").fadeOut("fast");
+    $("#players img").click(function() {
+        if ($(this).next(".hidden").is(":hidden")) {
+            $(this).next(".hidden").slideDown("normal");
+        } else {
+            $(this).next(".hidden").fadeOut("fast");
         }
     });
-  }
-);
+});
 
 //jQuery Simple Drop-Down Menu Plugin
 var timeout    = 300,
@@ -36,22 +31,9 @@ function jsddm_timer() {
 }
 
 function jsddm_canceltimer() {
-    if(closetimer)
-        {  window.clearTimeout(closetimer);
-            closetimer = null;
-        }
+    if (closetimer) {
+        window.clearTimeout(closetimer);
+        closetimer = null;
+    }
 }
 document.onclick = jsddm_close;
-
-//railscasts dynamic comments
-jQuery.ajaxSetup({
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};

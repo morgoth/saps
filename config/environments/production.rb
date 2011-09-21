14c73c7c9e6c9f6ee74a971b2a9b1e940f7b89e9
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2011-09-21 21:53:30 +0200

    added configuration for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,17 @@ Saps::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.default_url_options = {:host => "saps.heroku.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => "25",
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SENDGRID_DOMAIN']
+  }
 
   # Enable threaded mode
   # config.threadsafe!
